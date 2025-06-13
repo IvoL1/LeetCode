0b352bc3b221b9f1f9df36e0c5001b15c94d6253
Ivo Luis <ivoluis239@gmail.com>
2025-06-13 18:29:54 -0300

    Create Palindrome_Number.ts

---
+++ Easy/Palindrome_Number.ts
@@ -0,0 +1,7 @@
+// 9. Palindrome Number
+
+function isPalindrome(x: number): boolean {
+  const str = x.toString();
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+};
